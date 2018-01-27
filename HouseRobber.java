97b8fbad015e474df6107f6d453ff62f31f876e3
Ruchit Gandhi <gemini.ruchit@gmail.com>
2018-01-27 00:00:35 -0800

    Leetcode Practice

---
+++ HouseRobber.java
@@ -0,0 +1,21 @@
+public class HouseRobber {
+    public static void main(String[] args) {
+        int nums[] = {};
+        System.out.println(rob(nums));
+    }
+
+    private static int rob(int[] nums) {
+        int dp[] = new int[nums.length];
+        if (nums.length > 0) {
+            dp[0] = nums[0];
+            if (nums.length > 1) {
+                dp[1] = Math.max(nums[0], nums[1]);
+                for (int i = 2; i < nums.length; i++) {
+                    dp[i] = Math.max(nums[i] + dp[i - 2], dp[i - 1]);
+                }
+            }
+            return dp[nums.length - 1];
+        }
+        return 0;
+    }
+}
